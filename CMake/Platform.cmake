179e807ac26d14f716c65b9f3c5e5aca0c08506d
nevermore <trans-bug@outlook.com>
2024-08-12 23:09:21 +0800

    为cmake文件添加注释，添加可执行文件的task

--- CMake/Platform.cmake
+++ CMake/Platform.cmake
@@ -1,24 +1,23 @@
 ﻿# 在你的 CMakeLists.txt 文件中添加以下代码
-function(copy_qt_platform_plugins target_dir)
+function(copy_qt_platform_plugins target_dir)  # 定义复制Qt平台插件的函数
     # 获取 Qt 安装目录
-    get_filename_component(QT_PLUGINS_DIR "${Qt5_DIR}/../../../plugins" ABSOLUTE)
+    get_filename_component(QT_PLUGINS_DIR "${Qt5_DIR}/../../../plugins" ABSOLUTE)  # 获取Qt插件目录
 
     # 指定要复制的平台插件目录
-    set(QT_PLATFORMS_DIR "${QT_PLUGINS_DIR}/platforms")
+    set(QT_PLATFORMS_DIR "${QT_PLUGINS_DIR}/platforms")  # 设置平台插件目录
 
     # 确保目标目录存在
-    file(MAKE_DIRECTORY ${target_dir})
+    file(MAKE_DIRECTORY ${target_dir})  # 创建目标目录
 
     # 复制平台插件
-    file(COPY ${QT_PLATFORMS_DIR} DESTINATION ${target_dir})
+    file(COPY ${QT_PLATFORMS_DIR} DESTINATION ${target_dir})  # 复制平台插件
 
     # 添加自定义目标以在构建时触发插件复制
-    add_custom_target(copy-plugins
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${QT_PLATFORMS_DIR} ${target_dir}
-        DEPENDS ${target_dir}
+    add_custom_target(copy-plugins  # 添加自定义目标
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${QT_PLATFORMS_DIR} ${target_dir}  # 复制命令
+        DEPENDS ${target_dir}  # 依赖目标目录
     )
 
     # 使主目标依赖于自定义目标，确保在构建之前插件已经被复制
-    add_dependencies(${EXECUTABLE_NAME} copy-plugins)
+    add_dependencies(${EXECUTABLE_NAME} copy-plugins)  # 添加依赖关系
 endfunction()
-
--- CMake/StringListUtils.cmake
+++ CMake/StringListUtils.cmake
@@ -1,6 +1,6 @@
-function(list_add_prefix OUTPUT_LIST PREFIX)
-    foreach (item IN ITEMS ${ARGN})
-        list(APPEND RET "${PREFIX}${item}")
+function(list_add_prefix OUTPUT_LIST PREFIX)  # 定义添加前缀的函数
+    foreach (item IN ITEMS ${ARGN})  # 遍历列表
+        list(APPEND RET "${PREFIX}${item}")  # 添加前缀
     endforeach()
-    set(${OUTPUT_LIST} ${RET} PARENT_SCOPE)
+    set(${OUTPUT_LIST} ${RET} PARENT_SCOPE)  # 设置输出列表
 endfunction()
